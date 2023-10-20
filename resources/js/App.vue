e87e13804a17fc95b85db7f0a8415a25d72a51f2
KDdeveoper-git <kinda5749@gmail.com>
2023-10-20 16:52:04 +0700

    first commit -upload

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vueapp</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos12App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
